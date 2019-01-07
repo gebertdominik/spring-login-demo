fbd245537dfbd09bf7166a22c5a1ea2f17af727e
dgebert <dgebert91@gmail.com>
2019-01-07 23:37:29 +0100

    Add service

---
+++ src/main/java/com/dgebert/spring/security/loginDemo/service/UserService.java
@@ -0,0 +1,42 @@
+package com.dgebert.spring.security.loginDemo.service;
+
+import com.dgebert.spring.security.loginDemo.model.Role;
+import com.dgebert.spring.security.loginDemo.model.User;
+import com.dgebert.spring.security.loginDemo.repository.RoleRepository;
+import com.dgebert.spring.security.loginDemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service("userService")
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
+
+}
